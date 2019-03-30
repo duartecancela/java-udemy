cfd98617b088f62bcabf2d5d4818d42f6f36ff08
Duarte Cancela <duartecancela@gmail.com>
2019-03-30 22:52:18 +0000

    40

---
+++ CursoJava/src/controle/Switch.java
@@ -0,0 +1,26 @@
+package controle;
+
+public class Switch {
+    public static void main(String[] args) {
+        String faixa = "preta";
+
+        switch (faixa.toLowerCase()) {
+            case "preta":
+                System.out.println("Sei fazer o 'Bassai-Dai/Jion/Kanku-Dai'");
+            case "marrom":
+                System.out.println("Sei fazer o 'Tekki Shodan'");
+            case "roxa":
+                System.out.println("Sei fazer o 'Heian Godan'");
+            case "verde":
+                System.out.println("Sei fazer o 'Heian  Yondan'");
+            case "laranja":
+                System.out.println("Sei fazer o 'Heian  Sandan'");
+            case "vermelha":
+                System.out.println("Sei fazer o 'Heian  Nidan'");
+            case "amarela":
+                System.out.println("Sei fazer o 'Heian  Shodan'");
+            default:
+                System.out.println("Não sei nada");
+        }
+    }
+}

9b7b08d3fc7dcdbae004cf25111ab351c6086b10
Duarte Cancela <duartecancela@gmail.com>
2019-03-31 14:30:49 +0100

    Variáveis Locais

--- CursoJava/src/classe/DataTeste.java
+++ CursoJava/src/classe/DataTeste.java
@@ -3,13 +3,14 @@ package classe;
 public class DataTeste {
     public static void main(String[] args) {
         Data nascimento = new Data();
-        nascimento.dia = 19;
-        nascimento.mes = 11;
-        nascimento.ano = 1987;
+//        nascimento.dia = 19;
+//        nascimento.mes = 11;
+//        nascimento.ano = 1987;
 
         Data casamento = new Data(8, 9, 2010);
-        System.out.println(casamento.formatar());
         System.out.println(nascimento.formatar());
+        System.out.println(casamento.formatar());
+
     }
 }
 

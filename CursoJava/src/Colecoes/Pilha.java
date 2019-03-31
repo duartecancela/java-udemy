c540ae427548160bd1f19dbc8235c67a26f17989
Duarte Cancela <duartecancela@gmail.com>
2019-03-31 17:34:59 +0100

    Pilha

---
+++ CursoJava/src/Colecoes/Pilha.java
@@ -0,0 +1,21 @@
+package Colecoes;
+
+import java.util.Stack;
+
+public class Pilha {
+    public static void main(String[] args) {
+        Stack<String> pilha =  new Stack<>();
+
+        pilha.push("O Pequeno Principe");
+        pilha.push("O Hobbit");
+        pilha.push("Don Quixote");
+
+        System.out.println("Peek...");
+        System.out.println(pilha.peek());
+
+        System.out.println("pop...");
+        System.out.println(pilha.pop());
+        System.out.println(pilha.pop());
+        System.out.println(pilha.pop());
+    }
+}
